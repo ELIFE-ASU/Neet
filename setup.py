b4cb2e3d58f14c93199e428f9f630b07ba82ee11
Douglas Moore <doug@dglmoore.com>
2017-02-15 16:56:21 -0700

    Create the neet package
    The neet package provides several network models and algorithms
    to explore network dynamics.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name='neet',
+    version='0.0.0',
+    description='A brilliant and fundamental contribution to network science',
+    long_description=readme,
+    maintainer='Douglas G. Moore',
+    maintainer_email='doug@dglmoore.com',
+    url='https://github.com/elife-asu/neet',
+    license=license,
+    requires=[],
+    packages=['neet'],
+    platforms=['Windows', 'OS X', 'Linux'])
