e2357e06e3174a7290fa6f917164903ab7eb0223
Douglas Moore <doug@dglmoore.com>
2017-03-02 22:55:56 -0700

    Add open boundary conditions to ECAs

--- neet/ca.py
+++ neet/ca.py
@@ -6,10 +6,12 @@ class ECA(object):
     """
     ECA is a class to represent elementary cellular automaton rules. Each ECA
     contains an 8-bit integral member variable ``code`` representing the
-    Wolfram code for the ECA rule.
+    Wolfram code for the ECA rule and a set of boundary conditions which is
+    either ``None``, signifying periodic boundary conditions, or a pair of
+    cell states signifying fixed, open boundary conditions.
     """
 
-    def __init__(self, code):
+    def __init__(self, code, boundary=None):
         """
         Construct an elementary cellular automaton rule.
         
@@ -20,17 +22,34 @@ class ECA(object):
             >>> ca = ECA(30)
             >>> ca.code
             30
+            >>> ca.boundary
+            >>> ca = ECA(30, boundary=(0,0))
+            >>> ca.boundary
+            (0,0)
             
         :param code: the Wolfram code for the ECA
         :type code: int
+        :param boundary: the boundary conditions for the CA
+        :type boundary: tuple or None
         :raises TypeError: if ``code`` is not an instance of int
-        :raises ValueError: if ``code`` is not in :math:`\{0,1,\ldots,255\}`.
+        :raises ValueError: if ``code`` is not in :math:`\{0,1,\ldots,255\}`
+        :raises TypeError: if ``boundary`` is neither ``None`` or an instance of tuple
+        :raises ValueError: if ``boundary`` is a neither ``None`` or a pair of binary states
         """
         if not isinstance(code, int):
             raise(TypeError("ECA code is not an int"))
+        if boundary and not isinstance(boundary, tuple):
+            raise(TypeError("ECA boundary are neither None nor a tuple"))
         if 255 < code or code < 0:
             raise(ValueError("invalid ECA code"))
+        if boundary:
+            if len(boundary) != 2:
+                raise(ValueError("invalid ECA boundary conditions"));
+            for x in boundary:
+                if x != 0 and x != 1:
+                    raise(ValueError("invalid ECA boundary value"))
         self.code = code
+        self.boundary = boundary
 
     @classmethod
     def __check_arguments(self, lattice, n):
@@ -52,10 +71,11 @@ class ECA(object):
                 msg = "invalid value {} in lattice".format(x)
                 raise(ValueError(msg))
 
-    def __unsafe_update(self, lattice, n):
+    def __unsafe_update_closed(self, lattice, n):
         """
         Update the state of the ``lattice`` ``n``-times, in place, without
-        checking the validity of the arguments.
+        checking the validity of the arguments. This method uses closed
+        (a.k.a. periodic or cyclic) boundary conditions.
         
         :param lattice: the one-dimensional sequence of states
         :type lattice: seq
@@ -63,12 +83,31 @@ class ECA(object):
         :type n: int
         """
         for m in range(n):
-            a = lattice[0]
+            a = lattice[0]            
             d = 2 * lattice[-1] + lattice[0]
-            for i in range(1,len(lattice)):
+            for i in range(1, len(lattice)):
                 d = 7 & (2 * d + lattice[i])
                 lattice[i-1] = 1 & (self.code >> d)
             d = 7 & (2 * d + a)
+            lattice[-1] = 1 & (self.code >> d)
+            
+    def __unsafe_update_open(self, lattice, n):
+        """
+        Update the state of the ``lattice`` ``n``-times, in place, without
+        checking the validity of the arguments. This method uses fixed,
+        open boundary conditions.
+        
+        :param lattice: the one-dimensional sequence of states
+        :type lattice: seq
+        :param n: the number of times to update the state
+        :type n: int
+        """
+        for m in range(n):
+            d = 2 * self.boundary[0] + lattice[0]
+            for i in range(1, len(lattice)):
+                d = 7 & (2 * d + lattice[i])
+                lattice[i-1] = 1 & (self.code >> d)
+            d = 7 & (2 * d + self.boundary[1])
             lattice[-1] = 1 & (self.code >> d)
 
     def update(self, lattice, n=1):
@@ -80,17 +119,17 @@ class ECA(object):
         ::
         
             >>> ca = ECA(30)
-            >>> xs = [0,1,0]
-            >>> ca.update(xs)
-            >>> xs
-            [1, 1, 1]
-            >>> ca.update(xs)
-            >>> xs
-            [0, 0, 0]
             >>> xs = [0,0,1,0,0]
+            >>> ca.update(xs)
+            >>> xs
+            [0, 1, 1, 1, 0]
+            >>> ca.boundary = (0,1)
+            >>> ca.update(xs)
+            >>> xs
+            [1, 1, 0, 0, 0]
             >>> ca.update(xs, n=2)
             >>> xs
-            [1, 1, 0, 0, 1]
+            [1, 0, 1, 0, 1]
         
         :param lattice: the one-dimensional sequence of states
         :type lattice: seq
@@ -101,7 +140,10 @@ class ECA(object):
         :raises ValueError: unless :math:`lattice[i] \in \{0,1\}` for all :math:`i`
         """
         ECA.__check_arguments(lattice, n)
-        self.__unsafe_update(lattice, n)
+        if self.boundary:
+            self.__unsafe_update_open(lattice, n)
+        else:
+            self.__unsafe_update_closed(lattice, n)
 
     def step(self, lattice, n=1):
         """
@@ -112,18 +154,17 @@ class ECA(object):
         ::
         
             >>> ca = ECA(30)
-            >>> ca.step(xs)
-            [1, 1, 1]
-            >>> xs
-            [0, 1, 0]
-            >>> ca.step(xs, n=2)
-            [0, 0, 0]
             >>> xs = [0,0,1,0,0]
-            >>> ca.step(xs, n=2)
-            [1, 1, 0, 0, 1]
+            >>> ca.step(xs)
+            [0, 1, 1, 1, 0]
             >>> xs
             [0, 0, 1, 0, 0]
-        
+            >>> ca.boundary = (0,1)
+            >>> ca.step(xs)
+            [0, 1, 1, 1, 1]
+            >>> ca.step(xs, n=2)
+            [1, 1, 0, 0, 0]
+                    
         :param lattice: the one-dimensional sequence of states
         :type lattice: seq
         :param n: the number of times to update the state
@@ -135,5 +176,8 @@ class ECA(object):
         """
         ECA.__check_arguments(lattice, n)
         l = lattice[:]
-        self.__unsafe_update(l, n)
+        if self.boundary:
+            self.__unsafe_update_open(l, n)
+        else:
+            self.__unsafe_update_closed(l, n)
         return l
--- test/test_ca.py
+++ test/test_ca.py
@@ -17,12 +17,24 @@ class TestECA(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             ca.ECA("30")
+            
+        with self.assertRaises(TypeError):
+            ca.ECA(30, boundary=[1,2])
+            
+        with self.assertRaises(ValueError):
+            ca.ECA(30, boundary=(1,0,1))
+            
+        with self.assertRaises(ValueError):
+            ca.ECA(30, boundary=(1,2))
 
 
     def test_init(self):
         for code in range(256):
-            eca = ca.ECA(code)
-            self.assertEqual(code, eca.code)
+            for left in range(2):
+                for right in range(2):
+                    eca = ca.ECA(code, (left,right))
+                    self.assertEqual(code, eca.code)
+                    self.assertEqual((left,right), eca.boundary)
 
 
     def test_invalid_update_duration(self):
@@ -66,9 +78,9 @@ class TestECA(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             eca.update("101")
-
-
-    def test_update(self):
+        
+     
+    def test_update_closed(self):
         eca = ca.ECA(30)
 
         lattice = [0,0,1,0,0]
@@ -83,12 +95,35 @@ class TestECA(unittest.TestCase):
         self.assertEqual([1,1,1,0,0], lattice)
 
 
-    def test_update_long_time(self):
+    def test_update_open(self):
+        eca = ca.ECA(30, (0,1))
+
+        lattice = [0,0,1,0,0]
+
+        eca.update(lattice)
+        self.assertEqual([0,1,1,1,1], lattice)
+
+        eca.update(lattice)
+        self.assertEqual([1,1,0,0,0], lattice)
+
+        eca.update(lattice, n=2)
+        self.assertEqual([1,0,1,0,1], lattice)
+
+
+    def test_update_long_time_closed(self):
         eca = ca.ECA(45)
-        lattice = [1,1,0,1,0,0,1,0,1,0,0,1,0,1]
-        got = [0,1,1,0,1,0,1,0,1,0,1,0,1,0]
+        lattice  = [1,1,0,1,0,0,1,0,1,0,0,1,0,1]
+        expected = [0,1,1,0,1,0,1,0,1,0,1,0,1,0]
         eca.update(lattice, n=1000)
-        self.assertEqual(got, lattice)
+        self.assertEqual(expected, lattice)
+
+
+    def test_update_long_time_open(self):
+        eca = ca.ECA(45, (0,1))
+        lattice  = [1,1,0,1,0,0,1,0,1,0,0,1,0,1]
+        expected = [1,0,0,1,0,0,1,0,0,1,0,0,1,1]
+        eca.update(lattice, n=1000)
+        self.assertEqual(expected, lattice)
 
 
     def test_invalid_step_duration(self):
@@ -132,14 +167,14 @@ class TestECA(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             eca.update("101")
-
-
-    def test_step(self):
+            
+            
+    def test_step_closed(self):
         eca = ca.ECA(30)
 
         table = {1: [0,1,1,1,0],
-                2: [1,1,0,0,1],
-                4: [1,1,1,0,0]}
+                 2: [1,1,0,0,1],
+                 4: [1,1,1,0,0]}
 
         lattice = [0,0,1,0,0]
 
@@ -149,10 +184,33 @@ class TestECA(unittest.TestCase):
             self.assertEqual([0,0,1,0,0], lattice)
 
 
-    def test_step_long_time(self):
+    def test_step_open(self):
+        eca = ca.ECA(30, (1,0))
+
+        table = {1: [1,1,1,1,0],
+                 2: [0,0,0,0,1],
+                 4: [0,1,1,1,0]}
+
+        lattice = [0,0,1,0,0]
+
+        for n in table.keys():
+            got = eca.step(lattice, n=n)
+            self.assertEqual(table[n], got)
+            self.assertEqual([0,0,1,0,0], lattice)
+
+
+    def test_step_long_time_closed(self):
         eca = ca.ECA(45)
         lattice  = [1,1,0,1,0,0,1,0,1,0,0,1,0,1]
         expected = [0,1,1,0,1,0,1,0,1,0,1,0,1,0]
+        got = eca.step(lattice, n=1000)
+        self.assertEqual(expected, got)
+        
+
+    def test_step_long_time_open(self):
+        eca = ca.ECA(45, (0,1))
+        lattice  = [1,1,0,1,0,0,1,0,1,0,0,1,0,1]
+        expected = [1,0,0,1,0,0,1,0,0,1,0,0,1,1]
         got = eca.step(lattice, n=1000)
         self.assertEqual(expected, got)
 
