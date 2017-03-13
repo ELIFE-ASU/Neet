7b12a5b1e37edb8a3cb5f8e7d9b01f7ac22ed26d
Douglas Moore <doug@dglmoore.com>
2017-03-12 21:50:35 -0700

    Create _unsafe_update and update methods

--- neet/boolean.py
+++ neet/boolean.py
@@ -31,7 +31,9 @@ class WTNetwork(object):
         if n < 1:
             raise(ValueError("network must have at least one node"))
 
-        self.__size = n
+        self.__size       = n
+        self.__thresholds = np.zeros(n, dtype=np.int)
+        self.__weights    = np.zeros((n,n), dtype=np.int)
 
     @property
     def size(self):
@@ -73,5 +75,69 @@ class WTNetwork(object):
         """
         return StateSpace(self.size, b=2)
 
-    def update(self, lattice):
-        pass
+    def check_states(self, states):
+        """
+        Check the validity of the provided states.
+
+        .. rubric:: Examples;
+
+        ::
+
+            >>> WTNetwork(3).check_states([0,0,0])
+            True
+            >>> WTNetwork(3).check_states([0,0])
+            Traceback (most recent call last):
+                ...
+            ValueError: incorrect number of states in array
+            >>> WTNetwork(3).check_states([1,2,1])
+                ...
+            ValueError: invalid node state in states
+
+        :returns: ``True`` if the ``states`` are valid, otherwise an error is raised
+        :param states: the one-dimensional sequence of node states
+        :type states: sequence
+        :raises TypeError: if ``states`` is not iterable
+        :raises ValueError: if ``len(states)`` is not the number of nodes in the network
+        :raises ValueError: if ``states[i] not in [0,1]`` for any node ``i``
+        """
+        if len(states) != self.size:
+            raise(ValueError("incorrect number of states in array"))
+        for x in states:
+            if x !=0 and x != 1:
+                raise(ValueError("invalid node state in states"))
+        return True
+
+    def _unsafe_update(self, lattice):
+        """
+        Update ``states``, in place, according to the network update rules
+        without checking the validity of the arguments.
+
+        .. rubric:: Examples:
+
+        :param states: the one-dimensional sequence of node states
+        :type states: sequence
+        :returns: the updated states
+        """
+        temp = np.dot(self.__weights, lattice) - self.__thresholds
+        for (i,x) in enumerate(temp):
+            if x < 0.0:
+                lattice[i] = 0
+            elif x > 0.0:
+                lattice[i] = 1
+        return lattice
+
+    def update(self, states):
+        """
+        Update ``states``, in place, according to the network update rules.
+
+        .. rubric:: Examples:
+
+        :param states: the one-dimensional sequence of node states
+        :type states: sequence
+        :returns: the updated states
+        :raises TypeError: if ``states`` is not iterable
+        :raises ValueError: if ``len(states)`` is not the number of nodes in the network
+        :raises ValueError: if ``states[i] not in [0,1]`` for any node ``i``
+        """
+        self.check_states(states)
+        return self._unsafe_update(states)
