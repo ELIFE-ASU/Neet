512de3da6b17cbb1a5efd9de82ab1161b5520132
Douglas Moore <doug@dglmoore.com>
2017-06-13 12:11:27 -0700

    Create a basic implementation for async. transitions
    The current implementation is far from complete. It cannot handle
    decoded state output, non-fixed sized networks, or unchanging
    node states.
    
    We need more unit tests.

--- neet/asynchronous.py
+++ neet/asynchronous.py
@@ -1,3 +1,42 @@
 # Copyright 2017 ELIFE. All rights reserved.
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
+from .interfaces import is_network, is_fixed_sized
+from .statespace import StateSpace
+
+import copy
+
+def transitions(net):
+    """
+    Compute the asynchronous updates for a network.
+
+    The implementation only provides support for fixed sized networks and
+    includes all state updates, regardless of whether or not the update changed
+    the state of the node in question.
+
+    :param net: the network
+    :type net: network
+    :yields: a dictionary with encoded states and probabilities as keys and values
+    """
+    if not is_network(net):
+        raise TypeError("net must adhere to NEET's network interface")
+    # We need support for variable sized networks
+    if not is_fixed_sized(net):
+        raise ValueError("network must have a fixed size")
+
+    state_space = net.state_space()
+    size = net.size
+    # This will need to change when we consider unchanging node states
+    k = 1.0 / size
+    for state in state_space.states():
+        # A dictionary will not work when we admit non-encoded network states
+        next_states = dict()
+        for node in range(size):
+            next_state = copy.copy(state)
+            net.update(next_state, index=node)
+            state_code = state_space.encode(next_state)
+            if state_code in next_states:
+                next_states[state_code] += k
+            else:
+                next_states[state_code] = k
+        yield next_states
--- test/test_asynchronous.py
+++ test/test_asynchronous.py
@@ -3,7 +3,34 @@
 # license that can be found in the LICENSE file.
 import unittest
 from neet.asynchronous import *
+from neet.boolean.examples import s_pombe
 
 class TestAsync(unittest.TestCase):
+    class IsNetwork(object):
+        def update(self, lattice):
+            pass
+        @property
+        def size(self):
+            pass
+        def state_space(self):
+            return StateSpace(1)
+
+    class IsNotNetwork(object):
+        pass
+
     def test_canary(self):
         self.assertEqual(3, 1+2)
+
+    def test_transitions_not_network(self):
+        with self.assertRaises(TypeError):
+            list(transitions(5))
+
+        with self.assertRaises(TypeError):
+            list(transitions(self.IsNotNetwork()))
+
+        with self.assertRaises(TypeError):
+            list(transitions(self.IsNetwork))
+
+    def test_transitions_spombe(self):
+        for state in transitions(s_pombe):
+            self.assertAlmostEqual(1.0, sum(state.values()))
