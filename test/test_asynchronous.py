b8bc75d80991ba423d3657b21d36f06b6244a325
Douglas Moore <doug@dglmoore.com>
2017-06-19 21:56:40 -0700

    Update asynchronous unit test to include more networks

--- test/test_asynchronous.py
+++ test/test_asynchronous.py
@@ -2,35 +2,35 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 import unittest
-from neet.asynchronous import *
-from neet.boolean.examples import s_pombe
+from neet.asynchronous import transitions
+from neet.boolean.examples import s_pombe, s_cerevisiae, c_elegans
+from .mock import MockObject
 
 class TestAsync(unittest.TestCase):
-    class IsNetwork(object):
-        def update(self, lattice):
-            pass
-        @property
-        def size(self):
-            pass
-        def state_space(self):
-            return StateSpace(1)
-
-    class IsNotNetwork(object):
-        pass
-
+    """
+    Test the neet.asynchronous module
+    """
     def test_canary(self):
+        """
+        A canary test
+        """
         self.assertEqual(3, 1+2)
 
     def test_transitions_not_network(self):
+        """
+        Ensure that transitions fails when provided a non-network
+        """
         with self.assertRaises(TypeError):
             list(transitions(5))
 
         with self.assertRaises(TypeError):
-            list(transitions(self.IsNotNetwork()))
+            list(transitions(MockObject()))
 
-        with self.assertRaises(TypeError):
-            list(transitions(self.IsNetwork))
-
-    def test_transitions_spombe(self):
-        for state in transitions(s_pombe):
-            self.assertAlmostEqual(1.0, sum(state.values()))
+    def test_transitions_sum_to_one(self):
+        """
+        Ensure that the transition probabilities sum to one for each initial
+        state
+        """
+        for net in [s_pombe, s_cerevisiae, c_elegans]:
+            for state in transitions(net):
+                self.assertAlmostEqual(1.0, sum(state.values()))
