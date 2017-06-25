3799aecc09ae9f55cb1a94e0a91fb20ee09ad526
Siyu Zhou <siyu.zhou@outlook.com>
2017-06-24 21:33:40 -0700

    Fixed conflicts caused by improper import from `neet.landscape`

--- test/test_interfaces.py
+++ test/test_interfaces.py
@@ -3,13 +3,15 @@
 # license that can be found in the LICENSE file.
 import unittest
 from neet.interfaces import *
-from neet.landscape import StateSpace
+from neet.statespace import StateSpace
 import numpy as np
+
 
 class TestCore(unittest.TestCase):
     class IsNetwork(object):
         def update(self, lattice):
             pass
+
         def state_space(self):
             return StateSpace(1)
 
@@ -23,18 +25,20 @@ class TestCore(unittest.TestCase):
     class NotFixedSizedNetwork(IsNotNetwork):
         def size(self):
             return 5
-    
+
     class BaseThreeNetwork(object):
         def update(self, lattice):
             pass
+
         def state_space(self):
-            return StateSpace(1,b=3)
-    
+            return StateSpace(1, b=3)
+
     class MultipleBaseNetwork(object):
         def update(self, lattice):
             pass
+
         def state_space(self):
-            return StateSpace([1,2,3])
+            return StateSpace([1, 2, 3])
 
     def test_is_network(self):
         net = self.IsNetwork()
@@ -68,10 +72,9 @@ class TestCore(unittest.TestCase):
     def test_is_boolean_network(self):
         net = self.IsNetwork()
         self.assertTrue(is_boolean_network(net))
-        
+
         not_bool_net = self.BaseThreeNetwork()
         self.assertFalse(is_boolean_network(not_bool_net))
-        
+
         not_bool_net = self.MultipleBaseNetwork()
         self.assertFalse(is_boolean_network(not_bool_net))
-        
