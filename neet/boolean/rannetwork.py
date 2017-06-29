9b6442d6713ccf29b1eaca8b1d59ce74444552a6
Hyunju Kim <hkim78@asu.edu>
2017-06-28 20:09:21 -0700

    Removed unnecessary imports from the module, rannetwork

--- neet/boolean/rannetwork.py
+++ neet/boolean/rannetwork.py
@@ -2,10 +2,7 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 
-import sys
-import os
 import random as ran
-import networkx as nx
 import numpy as np
 import copy
 from .wtnetwork import WTNetwork
