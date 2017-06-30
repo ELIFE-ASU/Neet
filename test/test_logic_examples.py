579fabc6889308f7b9d5b06c8e2cca0a0224e734
Siyu Zhou <siyu.zhou@outlook.com>
2017-06-29 18:49:46 -0700

    Added logic table data file for myeloid network. Added test for file.

---
+++ test/test_logic_examples.py
@@ -0,0 +1,13 @@
+# Copyright 2017 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+"""Unit test for LogicNetwork examples"""
+import unittest
+import neet.boolean.examples as ex
+
+
+class TestLogicExamples(unittest.TestCase):
+    def test_myeloid(self):
+        self.assertEqual(11, ex.myeloid.size)
+        self.assertEqual(['GATA-2', 'GATA-1', 'FOG-1', 'EKLF', 'Fli-1', 'SCL',
+                          'C/EBPa', 'PU.1', 'cJun', 'EgrNab', 'Gfi-1'], ex.myeloid.names)
