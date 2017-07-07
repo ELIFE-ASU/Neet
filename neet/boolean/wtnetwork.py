c87527653b51a550beb5b3b19f363d53157c97a0
Douglas Moore <doug@dglmoore.com>
2017-07-07 10:46:13 -0700

    Add a values argument WTNetwork.update
    As part of the state-pinning process, we would like to be able to
    specify exactly which value to force a node's state into upon
    update.

--- neet/boolean/wtnetwork.py
+++ neet/boolean/wtnetwork.py
@@ -142,7 +142,7 @@ class WTNetwork(object):
         """
         return StateSpace(self.size, base=2)
 
-    def _unsafe_update(self, states, index=None, pin=None):
+    def _unsafe_update(self, states, index=None, pin=None, values=None):
         """
         Update ``states``, in place, according to the network update rules
         without checking the validity of the arguments.
@@ -220,16 +220,18 @@ class WTNetwork(object):
             temp = np.dot(self.weights, states) - self.thresholds
             self.theta(temp, states)
             if pin_states:
-                for (j,i) in enumerate(pin):
+                for (j, i) in enumerate(pin):
                     states[i] = pinned[j]
-            return states
         else:
             temp = np.dot(self.weights[index], states) - self.thresholds[index]
             states[index] = self.theta(temp, states[index])
-            return states
+        if values is not None:
+            for key in values:
+                states[key] = values[key]
+        return states
 
 
-    def update(self, states, index=None, pin=None):
+    def update(self, states, index=None, pin=None, values=None):
         """
         Update ``states``, in place, according to the network update rules.
 
@@ -308,10 +310,21 @@ class WTNetwork(object):
         if states not in self.state_space():
             raise ValueError("the provided state is not in the network's state space")
 
-        if (index is not None) and (pin is not None and pin != []):
-            raise ValueError("cannot provide both the index and pin arguments")
+        if index is not None:
+            if pin is not None and pin != []:
+                raise ValueError("cannot provide both the index and pin arguments")
+            elif values is not None and pin != {}:
+                raise ValueError("cannot provide both the index and values arguments")
+        elif pin is not None and values is not None:
+            for k in values.keys():
+                if k in pin:
+                    raise ValueError("cannot set a value for a pinned state")
+        if values is not None:
+            for val in values.values():
+                if val != 0 and val != 1:
+                    raise ValueError("invalid state in values argument")
 
-        return self._unsafe_update(states, index, pin)
+        return self._unsafe_update(states, index, pin, values)
 
     @staticmethod
     def read(nodes_file, edges_file):
