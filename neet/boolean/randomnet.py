2376600c6991fa80a5a6b746d8a5805cfebcee49
Siyu Zhou <siyu.zhou@outlook.com>
2017-08-16 10:38:28 -0700

    Added docstrings.

--- neet/boolean/randomnet.py
+++ neet/boolean/randomnet.py
@@ -1,3 +1,10 @@
+"""
+Functions that generate random networks from a given network.
+"""
+# Copyright 2017 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+
 import random
 from neet.statespace import StateSpace
 from .logicnetwork import LogicNetwork
@@ -5,6 +12,12 @@ from .logicnetwork import LogicNetwork
 
 def random_logic(logic_net, p=0.5, fixed_connections=True):
     """
+    Return a `LogicNetwork` from an input `LogicNetwork` with a random logic table.
+
+    :param logic_net: a :class:LogicNetwork
+    :param p: probability that a state is present in the activation table
+    :param fixed_connections: whether the returned network has the same connections as input
+    :returns: a random :class:LogicNetwork
     """
     if not isinstance(logic_net, LogicNetwork):
         raise ValueError('object must be a LogicNetwork')
@@ -17,6 +30,13 @@ def random_logic(logic_net, p=0.5, fixed_connections=True):
 
 def random_logic_fixed_connections(logic_net, p=0.5):
     """
+    Return a `LogicNetwork` from an input `LogicNetwork` with a random logic table.
+
+    Connections in the returned network are the same as those of the input.
+
+    :param logic_net: a :class:LogicNetwork
+    :param p: probability that a state is present in the activation table
+    :returns: a random :class:LogicNetwork
     """
     if not isinstance(logic_net, LogicNetwork):
         raise ValueError('object must be a LogicNetwork')
@@ -37,12 +57,19 @@ def random_logic_fixed_connections(logic_net, p=0.5):
 
 def random_logic_free_connections(logic_net, p=0.5):
     """
+    Return a `LogicNetwork` from an input `LogicNetwork` with a random logic table.
+
+    All possible connections within the network are considered in the random process.
+
+    :param logic_net: a :class:LogicNetwork
+    :param p: probability that a state is present in the activation table
+    :returns: a random :class:LogicNetwork
     """
     if not isinstance(logic_net, LogicNetwork):
         raise ValueError('object must be a LogicNetwork')
 
     new_table = []
-    for i in range(logic_net.size):
+    for _ in range(logic_net.size):
         n_indices = random.randint(1, logic_net.size)
         indices = tuple(sorted(random.sample(
             range(logic_net.size), k=n_indices)))
