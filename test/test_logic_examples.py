7a3c0f8e4480dfb736600e877fa54425f4bce794
Harrison Smith <hbs@asu.edu>
2017-10-23 10:04:33 -0700

    updated mouse cortical logic table, expression, and corresponding test/example files

--- test/test_logic_examples.py
+++ test/test_logic_examples.py
@@ -56,3 +56,89 @@ class TestLogicExamples(unittest.TestCase):
             if state not in attractors:
                 self.assertNotEqual(
                     ex.myeloid_from_expr.update(state[:]), state)
+
+    def test_mouse_cortical_7B(self):
+        self.assertEqual(10, ex.mouse_cortical_7B.size)
+        self.assertEqual(['gF', 'gE', 'gP', 'gC', 'gS', 'pF', 'pE', 'pP', 'pC', 'pS'], 
+                          ex.mouse_cortical_7B.names)
+        self.assertEqual(ex.mouse_cortical_7B.update([1, 1, 0, 0, 0, 0, 0, 0, 0, 0], 0),
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(ex.mouse_cortical_7B.update([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+                         [0, 1, 0, 1, 1, 1, 1, 0, 0, 0])
+        self.assertEqual(ex.mouse_cortical_7B.update([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+                         [0, 1, 0, 1, 1, 1, 1, 0, 0, 0])
+        self.assertEqual(ex.mouse_cortical_7B.update([0, 0, 0, 0, 0, 0, 0, 1, 1, 1]),
+                         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_mouse_cortical_7B_attractors(self):
+        # Assert attractors.
+        attractors = ([1, 0, 1, 0, 1, 1, 0, 1, 0, 1], # Anterior desired attractor (reachable from desired anterior initial state)
+                      [0, 1, 0, 1, 0, 0, 1, 0, 1, 0]) # Posterior desired attractor (unreachable from desired posterior initial state)
+
+        for state in attractors:
+            self.assertEqual(ex.mouse_cortical_7B.update(state[:]), state)
+
+        # Assert non-attractors.
+        for state in ex.mouse_cortical_7B.state_space():
+            if state not in attractors:
+                self.assertNotEqual(ex.mouse_cortical_7B.update(state[:]), state)
+
+    def test_mouse_cortical_7B_logic_expressions(self):
+        self.assertEqual(['gF', 'gE', 'gP', 'gC', 'gS', 'pF', 'pE', 'pP', 'pC', 'pS'],
+                         ex.mouse_cortical_7B_from_expr.names)
+        self.assertEqual(ex.mouse_cortical_7B_from_expr.table, ex.mouse_cortical_7B.table)
+        # Assert attractors.
+        attractors = ([1, 0, 1, 0, 1, 1, 0, 1, 0, 1], # Anterior desired attractor (reachable from desired anterior initial state)
+                      [0, 1, 0, 1, 0, 0, 1, 0, 1, 0]) # Posterior desired attractor (unreachable from desired posterior initial state)
+
+        for state in attractors:
+            self.assertEqual(ex.mouse_cortical_7B_from_expr.update(state[:]), state)
+
+        # Assert non-attractors.
+        for state in ex.mouse_cortical_7B.state_space():
+            if state not in attractors:
+                self.assertNotEqual(
+                    ex.mouse_cortical_7B_from_expr.update(state[:]), state)
+
+    def test_mouse_cortical_7C(self):
+        self.assertEqual(10, ex.mouse_cortical_7C.size)
+        self.assertEqual(['gF', 'gE', 'gP', 'gC', 'gS', 'pF', 'pE', 'pP', 'pC', 'pS'], 
+                          ex.mouse_cortical_7C.names)
+        self.assertEqual(ex.mouse_cortical_7C.update([1, 1, 0, 0, 0, 0, 0, 0, 0, 0], 0),
+                         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(ex.mouse_cortical_7C.update([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+                         [0, 1, 0, 1, 1, 1, 1, 0, 0, 0])
+        self.assertEqual(ex.mouse_cortical_7C.update([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+                         [0, 1, 0, 1, 1, 1, 1, 0, 0, 0])
+        self.assertEqual(ex.mouse_cortical_7C.update([0, 0, 0, 0, 0, 0, 0, 1, 1, 1]),
+                         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_mouse_cortical_7C_attractors(self):
+        # Assert attractors.
+        attractors = ([1, 0, 1, 0, 1, 1, 0, 1, 0, 1], # Anterior desired attractor (reachable from desired anterior initial state)
+                      [0, 1, 0, 1, 0, 0, 1, 0, 1, 0]) # Posterior desired attractor (unreachable from desired posterior initial state)
+
+        for state in attractors:
+            self.assertEqual(ex.mouse_cortical_7C.update(state[:]), state)
+
+        # Assert non-attractors.
+        for state in ex.mouse_cortical_7C.state_space():
+            if state not in attractors:
+                self.assertNotEqual(ex.mouse_cortical_7C.update(state[:]), state)
+
+    def test_mouse_cortical_7C_logic_expressions(self):
+        self.assertEqual(['gF', 'gE', 'gP', 'gC', 'gS', 'pF', 'pE', 'pP', 'pC', 'pS'],
+                         ex.mouse_cortical_7C_from_expr.names)
+        self.assertEqual(ex.mouse_cortical_7C_from_expr.table, ex.mouse_cortical_7C.table)
+        # Assert attractors.
+        attractors = ([1, 0, 1, 0, 1, 1, 0, 1, 0, 1], # Anterior desired attractor (reachable from desired anterior initial state)
+                      [0, 1, 0, 1, 0, 0, 1, 0, 1, 0]) # Posterior desired attractor (unreachable from desired posterior initial state)
+
+        for state in attractors:
+            self.assertEqual(ex.mouse_cortical_7C_from_expr.update(state[:]), state)
+
+        # Assert non-attractors.
+        for state in ex.mouse_cortical_7C.state_space():
+            if state not in attractors:
+                self.assertNotEqual(
+                    ex.mouse_cortical_7C_from_expr.update(state[:]), state)
