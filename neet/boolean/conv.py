5a49a77a4cbd94766a7f2dc065cdacee7088b8af
Siyu Zhou <siyu.zhou@outlook.com>
2017-12-18 12:08:35 -0700

    Added conversion from `WTNetwork` to `LogicNetwork`.

---
+++ neet/boolean/conv.py
@@ -0,0 +1,30 @@
+# Copyright 2017 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+from .logicnetwork import LogicNetwork
+from .wtnetwork import WTNetwork
+
+
+def wt_to_logic(net):
+    """Convert a WTNetwork to a LogicNetwork."""
+    if not isinstance(net, WTNetwork):
+        raise ValueError("'net' must be a WTNetwork")
+
+    truth_table = []
+    for node, weights in enumerate(net.weights):
+        indices = tuple([i for i, v in enumerate(weights)
+                         if v != 0 or i == node])
+
+        conditions = set()
+        for dec_state in range(2**len(indices)):
+            bin_state = '{0:0{1}b}'.format(dec_state, len(indices))
+            prod = sum([weights[i] * int(s)
+                        for i, s in zip(indices, bin_state)])
+            if (prod > net.thresholds[node]
+                    or (prod == net.thresholds[node]
+                        and bin_state[indices.index(node)] == '1')):
+                conditions.add(bin_state)
+
+        truth_table.append((indices, conditions))
+
+    return LogicNetwork(truth_table)
---
+++ test/test_conv.py
@@ -0,0 +1,26 @@
+# Copyright 2017 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+"""Unit tests for network conversion."""
+import unittest
+import neet.boolean.examples as ex
+import neet.boolean.conv as conv
+from neet.boolean.wtnetwork import WTNetwork
+from neet.boolean.logicnetwork import LogicNetwork
+
+
+class TestConv(unittest.TestCase):
+    def test_wt_to_logic(self):
+        net = conv.wt_to_logic(ex.s_pombe)
+        truth_table = [((0,), set()),
+                       ((1, 2, 3, 4), {'0000', '1000'}),
+                       ((0, 1, 2, 5, 8), {'00001', '00100',
+                                          '00101', '00111', '01101', '10101'}),
+                       ((0, 1, 3, 5, 8), {'00001', '00100',
+                                          '00101', '00111', '01101', '10101'}),
+                       ((4, 5), {'01', '11'}),
+                       ((2, 3, 4, 5, 6, 7), {'000001', '000101'}),
+                       ((1, 6, 8), {'001', '010', '011', '111'}),
+                       ((1, 7, 8), {'010', '100', '110', '111'}),
+                       ((4, 8), {'10', '11'})]
+        self.assertEqual(net.table, truth_table)
