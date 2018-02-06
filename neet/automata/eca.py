ead986646fa99b37748d0726ddb3c9eab1a2612f
Bryan Daniels <bryan.daniels.1@asu.edu>
2018-02-05 17:12:10 -0700

    import networkx where necessary

--- neet/automata/eca.py
+++ neet/automata/eca.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
 import numpy as np
+import networkx as nx
 from neet.statespace import StateSpace
 
 
