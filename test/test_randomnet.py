b48567b4aa0449971f72719eafd2b70b1223be8f
Bryan Daniels <bcdaniels@gmail.com>
2018-02-17 11:21:32 -0700

    add test for randomnet fixed-structure

---
+++ test/test_randomnet.py
@@ -0,0 +1,30 @@
+# Copyright 2017 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+import unittest
+from neet.boolean.examples import mouse_cortical_7B
+from neet.boolean.randomnet import *
+
+import numpy as np
+
+TESTSEED = 314159
+
+class TestRandomnet(unittest.TestCase):
+    
+
+    def test_random_logic_invalid_p(self):
+        """
+        ``random_logic`` should raise a value error if ``p`` is an incorrect size
+        """
+        with self.assertRaises(ValueError):
+            net = mouse_cortical_7B
+            random_logic(net,p=np.ones(net.size+1))
+
+    def test_random_logic_fixed_structure(self):
+        net = mouse_cortical_7B
+        np.random.seed(TESTSEED)
+        randnet = random_logic(net,connections='fixed-structure')
+        # fixed-structure should preserve all neighbors
+        for i in range(net.size):
+            self.assertEqual(net.neighbors_in(i),randnet.neighbors_in(i))
+
