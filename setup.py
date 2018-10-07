1db20e50aafe4a010654aca5c475865ae9604e73
Douglas G. Moore <doug@dglmoore.com>
2018-10-07 00:05:02 +0000

    Change requires to install_requires

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     maintainer_email='doug@dglmoore.com',
     url='https://github.com/elife-asu/neet',
     license=LICENSE,
-    requires=['numpy', 'networkx', 'pyinform'],
+    install_requires=['numpy', 'networkx', 'pyinform'],
     packages=['neet', 'neet.automata', 'neet.boolean'],
     package_data={'neet.boolean': ['data/*.txt', 'data/*.dat']},
     test_suite='test',
