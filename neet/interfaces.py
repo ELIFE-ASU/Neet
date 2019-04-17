b93c7a930b81cea7da2a922556e33649163e5356
Douglas G. Moore <doug@dglmoore.com>
2019-04-17 14:50:05 -0700

    Remove documentation from interfaces

--- neet/interfaces.py
+++ neet/interfaces.py
@@ -7,14 +7,6 @@
     from neet.interfaces import *
     from neet.statespace import StateSpace
 
-Interfaces
-==========
-
-The :mod:`neet.interfaces` module provides a collection of functions for
-determining if types adhere to various network interfaces, and generic
-functions for operating upon them. This done primarily through the
-and :func:`is_boolean_network` functions.
-
 API Documentation
 -----------------
 """
@@ -54,37 +46,6 @@ class Network(object):
         pass
 
     def neighbors(self, index, direction='both', *args, **kwargs):
-        """
-        Return a set of neighbors for a specified node, or a list of sets of
-        neighbors for all nodes in the network.
-
-        For ECAs, in the cases of the lattices having fixed boundary conditions,
-        the left boundary, being on the left of the leftmost index 0, has an index
-        of -1, while the right boundary's index is the size+1. The full state of
-        the lattices and the boundaries is equavolent to: `[cell0, cell1, ...,
-        cellN, right_boundary, left_boundary]` if it is ever presented as a single
-        list in Python.
-
-        :param index: node index, if neighbors desired for one node only
-        :param direction: type of node neighbors to return ('in', 'out', or 'both')
-        :kwarg size: size of ECA, required if network is an ECA
-        :returns: a set of neighbors of a node
-
-        .. rubric:: Example
-
-        .. doctest:: interfaces
-
-            >>> net = ECA(30)
-            >>> neighbors(net, index=2, size=3, direction='out')
-            {0, 1, 2}
-            >>> net.boundary = (1,1)
-            >>> neighbors(net, index=2, size=3, direction='out')
-            {1, 2}
-
-        See `ECA.neighbors()`,`LogicNetwork.neighbors()` or `WTNetwork.neighbors()`
-        docstrings for more details and basic use examples.
-
-        """
         if direction not in ('in', 'out', 'both'):
             raise ValueError('direction must be "in", "out" or "both"')
 
