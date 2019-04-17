5acc75e974afa4ae3269517e4928d4af3c462f39
Douglas G. Moore <doug@dglmoore.com>
2019-04-16 23:33:23 -0700

    Use six to add metaclass
    Python2 and Python3 have different syntax for adding metaclasses. The
    only portable way that I can find to handle this is using the
    `@six.add_metaclass` decorator. Seems to work in Python3.  We'll find
    out if it works in Python2 when I push.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     maintainer_email='doug@dglmoore.com',
     url='https://github.com/elife-asu/neet',
     license=LICENSE,
-    install_requires=['numpy', 'networkx', 'pyinform'],
+    install_requires=['six', 'numpy', 'networkx', 'pyinform'],
     setup_requires=['green'],
     packages=['neet', 'neet.automata', 'neet.boolean'],
     package_data={'neet.boolean': ['data/*.txt', 'data/*.dat']},
