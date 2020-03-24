25691f538ba01aeb20b608e0d2b016cf1502bbfc
Ariana Kiaei <akiaei@asu.edu>
2020-03-24 09:46:31 -0700

    Added folder and file for the random net tests

---
+++ test/boolean/random/test_constraints.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestConstraints(unittest.TestCase):
+    def testNum(self):
+        #just filler to check that this test gets run with the other unit tests
+        x = 3
+        self.assertEqual(x, 3)
